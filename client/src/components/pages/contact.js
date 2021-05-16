dcf96af09ee1cf7da43d92d15ea9bf98a9177764
Magdi Tiea <magditiea@gmail.com>
2021-05-16 16:42:54 -0400

    nodemailer finalized and success upon sending an email from the client side form

--- client/src/components/pages/contact.js
+++ client/src/components/pages/contact.js
@@ -24,7 +24,7 @@ const Contact = () => {
   const submitEmail = async (e) => {
     e.preventDefault();
     console.log({ mailerState });
-    const response = await fetch("http://localhost:8080/send", {
+    const response = await fetch("http://localhost:3001/send", {
       method: "POST",
       headers: {
         "Content-type": "application/json",
@@ -49,6 +49,7 @@ const Contact = () => {
         });
       });
   };
+ 
 
 
   return (
