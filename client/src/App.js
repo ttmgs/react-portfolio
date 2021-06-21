c900fffc754a7024771d6294cc3587e3704e6e42
Magdi Tiea <magditiea@gmail.com>
2021-06-21 00:48:07 -0400

    route changed

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
   <div>
 <Nav />
 <Route exact path="/react-portfolio" component={About}   />
-
+<Route exact path="/projects" component={Projects}  />
 <Route exact path="/react-portfolio/projects" component={Projects}  />
 
 <Footer/>
