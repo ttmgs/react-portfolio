d9a692b7f3847d538adc6bebe13688355aa54026
Magdi Tiea <magditiea@gmail.com>
2021-09-16 08:31:06 -0400

    added server

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Send every request to the React app
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
+
+
+
+
+
+
+// custom domain name added and loading
+// react app works deployed
