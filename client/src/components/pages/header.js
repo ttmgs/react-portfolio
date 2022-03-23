1adac7523c2c4352162db17403dfc32fff8c26d1
Magdi <ttmgs@Magdis-MacBook-Air.local>
2022-03-23 06:28:27 -0400

    edit

--- client/src/components/pages/header.js
+++ client/src/components/pages/header.js
@@ -23,7 +23,7 @@ function Header() {
         </div>
         <div class="col-lg-6 mx-auto">
           <p class="lead mb-4" style={{color: "white"}}>Full Stack Web Developer<span style={{color: 'black'}}> </span></p>
-       <a href="https://www.youtube.com/channel/UCrsmLM2oLKG89apiD3EaQgg"><i class="fa fa-youtube-play" style={{fontSize: '48px', color:'red'}}></i></a>
+       <a href="https://www.youtube.com/channel/UC4zlONKz1PkBAE2pByoI0lQ"><i class="fa fa-youtube-play" style={{fontSize: '48px', color:'red'}}></i></a>
 
           <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
           {/* <a
