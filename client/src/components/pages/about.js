f3b938040e09c255cfff27d7ade124338d2d2240
Magdi <ttmgs@Magdis-MacBook-Air.local>
2022-03-25 09:07:49 -0400

    edit

--- client/src/components/pages/about.js
+++ client/src/components/pages/about.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import "./home.css";
 import grad from "../images/grad.png";
 import upei from "../images/upei.png";
@@ -10,55 +10,81 @@ import "aos/dist/aos.css";
 
 
 function About() {
-  useEffect(() => {
-    Aos.init({ duration: 1000});
-  }, [])
+    useEffect(() => {
+        Aos.init({ duration: 1000 });
+    }, [])
 
 
-  return (
-    <div>
+    return ( <
+        div >
 
-<div id="about" style={{backgroundColor: "black"}}>
-<div class="b-example-divider"></div>
-    
-    <div class="px-4 pt-5 my-5 text-center border-bottom" style={{color: "white"}}>
-      <h1 class="display-4 fw-bold">About Me</h1>
-      <div class="col-lg-6 mx-auto">
-        <p class="lead mb-4" style={{color: "white"}}> Focused and quick learning Full Stack Web Developer with 3 years of experience 
-        in computer science, programming, and UX design for various projects.</p>
-        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-          <center>
-        <img
-              src={grad}
-              alt="grad"
-              style={{ width: "140px", height: "140px", marginBottom: "15px" }}
-            ></img>
-            <img
-              src={mom}
-              alt="grad"
-              style={{ width: "140px", height: "140px", marginBottom: "15px" }}
-            ></img>
-            <img
-              src={me}
-              alt="grad"
-              style={{ width: "140px", height: "140px", marginBottom: "15px" }}
-            ></img>
-            </center>
-        </div>
-      </div>
-      <div class="overflow-hidden" style={{maxHeight: "30vh"}}>
-        <div class="container px-5" style={{display: "inline-flex"}}>
-       
-        </div>
-      </div>
-    </div>
-</div>
-    </div>
-  );
+        <
+        div id = "about"
+        style = {
+            { backgroundColor: "black" }
+        } >
+        <
+        div class = "b-example-divider" > < /div>
+
+        <
+        div class = "px-4 pt-5 my-5 text-center border-bottom"
+        style = {
+            { color: "white" }
+        } >
+        <
+        h1 class = "display-4 fw-bold" > About Me < /h1> <
+        div class = "col-lg-6 mx-auto" >
+        <
+        p class = "lead mb-4"
+        style = {
+            { color: "white" }
+        } > Focused and quick learning Full Stack Web Developer who enjoys computer science, programming, and UX design
+        for various projects. < /p> <
+        div class = "d-grid gap-2 d-sm-flex justify-content-sm-center mb-5" >
+        <
+        center >
+        <
+        img src = { grad }
+        alt = "grad"
+        style = {
+            { width: "140px", height: "140px", marginBottom: "15px" }
+        } >
+        <
+        /img> <
+        img src = { mom }
+        alt = "grad"
+        style = {
+            { width: "140px", height: "140px", marginBottom: "15px" }
+        } >
+        <
+        /img> <
+        img src = { me }
+        alt = "grad"
+        style = {
+            { width: "140px", height: "140px", marginBottom: "15px" }
+        } >
+        <
+        /img> < /
+        center > <
+        /div> < /
+        div > <
+        div class = "overflow-hidden"
+        style = {
+            { maxHeight: "30vh" }
+        } >
+        <
+        div class = "container px-5"
+        style = {
+            { display: "inline-flex" }
+        } >
+
+        <
+        /div> < /
+        div > <
+        /div> < /
+        div > <
+        /div>
+    );
 }
 
 export default About;
-
-
-
-
--- client/src/components/pages/header.js
+++ client/src/components/pages/header.js
@@ -1,5 +1,6 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import logo from "../images/cropme.png";
+import resume from "../images/resume.pdf"
 import "./home.css";
 import Aos from "aos";
 import "aos/dist/aos.css";
@@ -8,34 +9,67 @@ import "aos/dist/aos.css";
 
 function Header() {
 
-  
-  return(
 
-    <div id="home">
+    return (
 
-      <div class="px-4 py-5 my-5 text-center">
-        
-        <img class="d-block mx-auto mb-4" src={logo} alt="" width="300" height="300"/>
-        <div className="center">
-        <div className="typewriter">
-        <h1 class="display-5 fw-bold" style={{color: "blue"}}> Magdi Tiea</h1>
-        </div>
-        </div>
-        <div class="col-lg-6 mx-auto">
-          <p class="lead mb-4" style={{color: "white"}}>Full Stack Web Developer<span style={{color: 'black'}}> </span></p>
-       <a href="https://www.youtube.com/channel/UC4zlONKz1PkBAE2pByoI0lQ"><i class="fa fa-youtube-play" style={{fontSize: '48px', color:'red'}}></i></a>
+        <
+        div id = "home" >
 
-          <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          {/* <a
-          class="btn line-btn-dark btn-icon btn-radius"
-          href={resume}
-          title=""
-          download
-        >   <button type="submit" class="btn btn-primary btn-lg px-4 gap-3" style={{display: "block"}}>Click to Download resume</button></a> */}
-          </div>
-        </div>
-      </div>
-      </div>
+        <
+        div class = "px-4 py-5 my-5 text-center" >
+
+        <
+        img class = "d-block mx-auto mb-4"
+        src = { logo }
+        alt = ""
+        width = "300"
+        height = "300" / >
+        <
+        div className = "center" >
+        <
+        div className = "typewriter" >
+        <
+        h1 class = "display-5 fw-bold"
+        style = {
+            { color: "blue" }
+        } > Magdi Tiea < /h1> < /
+        div > <
+        /div> <
+        div class = "col-lg-6 mx-auto" >
+        <
+        p class = "lead mb-4"
+        style = {
+            { color: "white" }
+        } > Full Stack Web Developer < span style = {
+            { color: 'black' }
+        } > < /span></p >
+        <
+        div style = {
+            { flexDirection: "row" }
+        } >
+        <
+        h4 > Youtube Channel: < a href = "https://www.youtube.com/channel/UC4zlONKz1PkBAE2pByoI0lQ" > < i class = "fa fa-youtube-play"
+        style = {
+            { fontSize: '48px', color: 'red' }
+        } > < /i></a > < /h4>  < /
+        div > <
+        div class = "d-grid gap-2 d-sm-flex justify-content-sm-center" >
+        <
+        a class = "btn line-btn-dark btn-icon btn-radius"
+        href = { resume }
+        title = ""
+        download >
+        <
+        button type = "submit"
+        class = "btn btn-primary btn-lg px-4 gap-3"
+        style = {
+            { display: "block" }
+        } > Click to Download resume < /button></a >
+        <
+        /div> < /
+        div > <
+        /div> < /
+        div >
 
 
 
@@ -54,7 +88,7 @@ function Header() {
 
 
 
-  )
+    )
 }
 
 export default Header;
